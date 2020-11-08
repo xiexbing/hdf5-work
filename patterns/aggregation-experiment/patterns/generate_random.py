16e998a9e42f9bd3203761622206bbcddbf2bc01
Bing Xie <xiebingbing@gmail.com>
2020-11-08 11:43:59 -0500

    new file:   patterns/aggregation-experiment/patterns/benchmark_pattern/cori/node16/hints/aggr_16_16M 	new file:   patterns/aggregation-experiment/patterns/benchmark_pattern/cori/node16/hints/aggr_16_1M 	new file:   patterns/aggregation-experiment/patterns/benchmark_pattern/cori/node16/hints/aggr_16_256M 	new file:   patterns/aggregation-experiment/patterns/benchmark_pattern/cori/node16/hints/aggr_16_4M 	new file:   patterns/aggregation-experiment/patterns/benchmark_pattern/cori/node16/hints/aggr_16_64M

---
+++ patterns/aggregation-experiment/patterns/generate_random.py
@@ -0,0 +1,157 @@
+import os
+import numpy as np
+import shutil
+import math
+
+
+machines = ['summit', 'cori']
+nodes = [4, 8, 16, 32]
+per_set = 20
+rdir = "benchmark_pattern"
+
+def generate_pattern():
+
+    for machine in machines:
+        for node in nodes:
+            sizeGroups = generate_sizes() 
+            nodeSizes = []
+
+            for sizeSum in sizeGroups:
+                [sizeGroup, sizeRange, sizeName] = sizeSum
+                [sizeMin, sizeMax] = sizeRange 
+                for i, size in enumerate(sizeGroup):
+                    confirm = 0
+                    while confirm == 0:
+                        if machine == 'summit':                    
+                            core = np.random.randint(1, 42, 1)[0]
+                        elif machine == 'cori':                    
+                            core = np.random.randint(1, 32, 1)[0]
+
+                        #data size per core
+                        per_size = math.ceil(size/core)
+                        node_size = per_size *core
+                        if node_size >= sizeMin and node_size <= sizeMax and node_size not in nodeSizes and per_size < 1024*1024:
+                            nodeSizes.append(node_size)
+                            per_pattern(machine, sizeName, node, core, per_size)
+                            confirm = 1
+
+            generate_hints(machine, node)    
+
+def generate_hints(machine, node):
+
+    hdir = rdir + '/' + machine + '/node' + str(node) + '/hints'
+    naggrs = [int(node*4), int(node*2), int(node), int(node/2), int(node/4)]
+    buffSizes = [1, 4, 16, 64, 256]
+    unit = 1024*1024
+
+    #config list
+    if not os.path.isdir(hdir):
+        os.makedirs(hdir)
+    for n in naggrs:
+        for size in buffSizes:
+            aggr = str(n)
+            bsize = str(size * unit)
+            aggrLine = 'cb_nodes ' + aggr + '\n'
+            sizeLine = 'cb_buffer_size ' + bsize + '\n'
+            wLine = 'romio_cb_write enable' + '\n'
+            rLine = 'romio_cb_read enable' + '\n'
+            name = 'aggr_' + aggr + '_' + str(size) + 'M'
+            f = os.path.join(hdir, name)
+            hf = open(f, 'a')
+            hf.write(aggrLine)
+            hf.write(sizeLine)
+            hf.write(wLine)
+            hf.write(rLine)
+            hf.truncate()
+            hf.close() 
+
+    aggrLine = 'cb_nodes ' + str(node) + '\n'
+    sizeLine = 'cb_buffer_size 16777216' + '\n'
+    wLine = 'romio_cb_write automatic' + '\n'
+    rLine = 'romio_cb_read automatic' + '\n'
+    name = 'default' 
+    f = os.path.join(hdir, name)
+    hf = open(f, 'a')
+    hf.write(aggrLine)
+    hf.write(sizeLine)
+    hf.write(wLine)
+    hf.write(rLine)
+    hf.truncate()
+    hf.close()
+
+                        
+def generate_sizes():
+
+    #aggregate size per node 1KB --- 4MB (4096KB) 
+    n1 = np.random.randint(1, 4*1024, per_set)
+
+    #aggregate size per node 4097KB --- 16MB (16*1024KB) 
+    n2 = np.random.randint(4*1024+1, 16*1024, per_set)
+
+    #aggregate size per node 16*1024+1 KB --- 64MB (64*1024KB)
+    n3 = np.random.randint(16*1024+1, 64*1024, per_set)
+
+    #aggregate size per node 64*1024+1 KB --- 256MB (256*1024KB) 
+    n4 = np.random.randint(64*1024+1, 256*1024, per_set)
+
+    #aggregate size per node 256*1024+1 KB --- 1GB (1024*1024KB) 
+    n5 = np.random.randint(256*1024+1, 1024*1024, per_set)
+
+    #aggregate size per node 1024*1024+1 KB --- 4GB (4*1024*1024KB) 
+    n6 = np.random.randint(1024*1024+1, 4*1024*1024, per_set)
+
+
+    node_sizes = [[n1, [1, 4*1024], 'n1'], [n2, [4*1024+1, 16*1024], 'n2'],  [n3, [16*1024+1, 64*1024], 'n3'],  [n4, [64*1024+1, 256*1024], 'n4'],  [n5, [256*1024+1, 1024*1024], 'n5'],  [n6, [1024*1024+1, 4*1024*1024], 'n6'] ]
+
+    return node_sizes
+
+def per_pattern(machine, sizeName, node, core, per_size):
+
+    name = sizeName + "_" + str(core) + "_" + str(per_size) + "k"
+    ndir = rdir + '/' + machine + '/node' + str(node) 
+    
+    if not os.path.isdir(ndir):
+        os.makedirs(ndir)
+    fname = os.path.join(ndir, name) 
+    ffile = open(fname, 'a')
+
+    iline = '\n' + "for i in $(seq 1 1 3); do" + '\n'
+    score = "'" + str(core) + "'"
+    coreline = "for ncore in " + score + "; do" + '\n'
+    ssize = "'" + str(per_size) + "k" + "'"
+    sizeline = "for burst in " + ssize + "; do" + '\n'
+    stripeLine = "for stripe_size in $stripe_sizes; do" + '\n'
+
+    iorline = "ior $i $ncore $burst" + '\n'
+    coriline = "ior $i $ncore $burst $stripe_size" + '\n'
+
+
+    doneline = "done" + '\n'
+    recordline = "echo " + name + " done" + '\n'
+    ffile.write(iline)
+    ffile.write(coreline)
+    ffile.write(sizeline)
+    if machine == 'cori':
+        ffile.write(stripeLine)
+    if machine == 'summit':
+        ffile.write(iorline)
+    else:
+        ffile.write(coriline)
+    ffile.write(doneline)
+    ffile.write(doneline)
+    ffile.write(doneline)
+    if machine == 'cori':
+        ffile.write(doneline)
+    ffile.write(recordline)
+
+         
+
+def main():
+
+    if os.path.isdir(rdir):
+        shutil.rmtree(rdir)
+
+    generate_pattern()
+
+main()
+
---
+++ patterns/aggregation-experiment/template/summit/template.sh
@@ -0,0 +1,170 @@
+#!/bin/bash -l
+#BSUB -P stf008 
+#BSUB -W 2:00
+#BSUB -nnodes NNODE
+##BSUB -w ended(PREVJOBID)
+# #BSUB -alloc_flags gpumps
+#BSUB -J IOR_NNODEnode
+#BSUB -o o%J.ior_NNODEnode
+#BSUB -e o%J.ior_NNODEnode
+
+CDIR=ior_data
+EXEC=/gpfs/alpine/stf008/scratch/bing/ior_mod/src/ior
+LD_LIBRARY_PATH=/gpfs/alpine/csc300/world-shared/gnu_build/hdf5-1.10.6.mod/build/hdf5/lib:$LD_LIBRARY_PATH
+
+#enable darshan dxt trace 
+export MPICH_MPIIO_STATS=1
+export MPICH_MPIIO_HINTS_DISPLAY=1
+export MPICH_MPIIO_TIMERS=1
+export DARSHAN_DISABLE_SHARED_REDUCTION=1
+export DXT_ENABLE_IO_TRACE=4
+
+#print romio hints
+export ROMIO_PRINT_HINTS=1
+
+#two varying parameters: 1. number of aggregators, 2. read/write buffer size
+half_aggr=$((NNODE/2))
+quat_aggr=$((NNODE/4))
+eqal_aggr=NNODE
+
+naggrs="$eqal_aggr $half_aggr $quat_aggr"
+buff_sizes="1M 4M 16M 64M 256M"
+
+ior(){
+    local i=$1
+    local ncore=$2
+    local burst=$3
+ 
+    #check file size to determine alignment setting
+    size="${burst//k}"
+    fileSize=$(($size*$ncore*NNODE/1024))
+
+    if [[ $fileSize -ge 16 ]]; then
+        align=16m
+    else
+        align=1m
+    fi
+
+    rdir=result_${ncore}_${burst}
+    mkdir -p $rdir 
+    mkdir -p $CDIR
+ 
+    write(){
+        ind_write(){
+            #independent write
+            #flush data in data transfer, before file close
+            jsrun -n NNODE -r 1 -a $ncore -c $ncore $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -e -w -o $CDIR/ind_${i}_${ncore}_${burst}_f&>>$rdir/ind_${ncore}_${burst}_f --hdf5.collectiveMetadata 
+        }
+
+        col_write(){
+            local naggr=$1
+            local buffer=$2  
+
+            hfile=$rdir/aggr_${naggr}_${buffer}_${ncore}
+            if [[ ! -f $hfile ]]; then
+                cp hints/aggr_${naggr}_${buffer} $hfile  
+                list="cb_config_list "
+                if [[ $naggr -le NNODE ]]; then
+                    ag(){
+                        local na=$1
+                        rank=$((($na-1)*$ncore))
+                        list=$list" $rank"
+                    }
+                    for na in $(seq 1 1 $naggr); do
+                        ag $na
+                    done
+                else
+                    daggr=NNODE
+                    dg(){
+                        local na=$1
+                        inNode=$(($naggr/NNODE))
+                        per(){
+                            local p=$1
+                            n=$((($na-1)*$ncore))
+                            rank=$(($n+$p-1))
+                            list=$list" $rank"
+                        }
+                        for p in $(seq 1 1 $inNode); do
+                            per $p
+                        done
+                   }
+                   for na in $(seq 1 1 NNODE); do
+                       dg $na
+                   done
+                fi
+                echo $list>>$hfile
+            fi
+
+            #load romio hints
+            export ROMIO_HINTS=$hfile
+
+            #flush data in data transfer, before file close 
+            jsrun -n NNODE -r 1 -a $ncore -c $ncore $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -e -w -o $CDIR/col_${i}_${ncore}_${burst}_${naggr}_${buffer}_f&>>$rdir/col_${ncore}_${burst}_${naggr}_${buffer}_f --hdf5.collectiveMetadata
+        }
+
+        default_write(){
+            #load romio hints
+            cp hints/default $rdir/.
+            export ROMIO_HINTS=$rdir/default
+
+            #flush data in data transfer, before file close 
+            jsrun -n NNODE -r 1 -a $ncore -c $ncore $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -e -w -o $CDIR/col_${i}_${ncore}_${burst}_default_f&>>$rdir/col_${ncore}_${burst}_default_f --hdf5.collectiveMetadata
+        }
+
+
+        for naggr in $naggrs; do
+            for buffer in $buff_sizes; do
+                col_write $naggr $buffer
+            done
+        done
+
+        default_write
+        ind_write
+    }
+    
+
+    read(){
+        ind_read(){
+            #independent read
+            jsrun -n NNODE -r 1 -a $ncore -c $ncore $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -r -Z -o $CDIR/ind_${i}_${ncore}_${burst}_f&>>ind_${ncore}_${burst}_r --hdf5.collectiveMetadata     
+        }
+
+        col_read(){ 
+            local naggr=$1
+            local buffer=$2  
+
+            hfile=$rdir/aggr_${naggr}_${buffer}_${ncore}
+            #load romio hints
+            export ROMIO_HINTS=$rdir/aggr_${naggr}_${buffer}
+            jsrun -n NNODE -r 1 -a $ncore -c $ncore $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -r -Z -o $CDIR/col_${i}_${ncore}_${burst}_${naggr}_${buffer}_f&>>$rdir/col_${ncore}_${burst}_${naggr}_${buffer}_r --hdf5.collectiveMetadata      
+        }
+        for naggr in $naggrs; do
+            for buffer in $buff_sizes; do
+                col_read $naggr $buffer
+            done
+        done
+ 
+        default_read(){ 
+            #load romio hints
+            export ROMIO_HINTS=$rdir/default
+            jsrun -n NNODE -r 1 -a $ncore -c $ncore $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -r -Z -o $CDIR/col_${i}_${ncore}_${burst}_default_f&>>$rdir/col_${ncore}_${burst}_default_r --hdf5.collectiveMetadata       
+        }
+ 
+        for naggr in $naggrs; do
+            for buffer in $buff_sizes; do
+                col_read $naggr $buffer
+            done
+        done
+    
+        default_read
+        ind_read
+  
+    }
+
+    write
+    read
+
+    #clean the run
+    rm -rf $CDIR
+}
+
