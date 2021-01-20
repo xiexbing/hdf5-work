6606aa264dd8e0fbf57f2c1e1533f9de1a4ecc22
Houjun Tang <htang4@lbl.gov>
2021-01-19 17:45:59 -0800

    Update theta scripts

--- patterns/aggregation-experiment/template/theta/build.sh
+++ patterns/aggregation-experiment/template/theta/build.sh
@@ -1,80 +1,81 @@
 #!/bin/bash
 
-
 nodes="4"
 user=$USER
-num_running_jobs=1
+
+repetitions=9
+per_write=3
 
 cdir=~/hdf5-work/patterns/aggregation-experiment/patterns/benchmark_pattern/
 machine=theta
 idir=$cdir/$machine
 #determine how many jobs in the queue
-count=`qstat|grep $USER|wc -l`
+count=`qstat|grep $USER|grep IOR|wc -l`
 curr_dir=`pwd`
+num_running_jobs=1
 
-first_submit=1
 node(){
     local node=$1
     ndir=node${node}
     pdir=$idir/$ndir
+    
     mkdir -p $ndir
     cp -rf $pdir/hints $ndir/.
     cp template.sh $ndir/.
+    touch $ndir/complete
 
     patterns=`ls $pdir|sed "s/hints//"`   
     cd $ndir
 
     #check completed pattern
-    complete=`cat complete`
-
     pattern(){
         local per=$1
-        local num=0
-        check_complete(){
-            local record=$1
-            if [[ "$record" == "$per" ]]; then
-                num=$(($num + 1)) 
-            fi
-        }
-        for record in $complete; do
-            check_complete $record
-        done
-
-        if [[ $num -lt 3 ]]; then
-            local timestamp=`date +%s`
-            local name=${per}_${timestamp}.sh
-
-            cp template.sh $name
-
-	    if [ "$node" -gt "16" ]
-	    then 
-		sed -i "s/ALLOCNODE/128/g" $name 
-		sed -i "s/NNODE/$node/g" $name 
-		sed -i "s/MYQUEUE/default/g" $name 
-	    else
-		sed -i "s/ALLOCNODE/$node/g" $name 
-		sed -i "s/NNODE/$node/g" $name 
-		sed -i "s/MYQUEUE/debug-cache-quad/g" $name 
-	    fi
-
-
-
-            cat $pdir/$per>>$name
-            sed -i -e "s/NNODE/$node/g" ${name}
-            echo "echo \"====Done====\"" >> $name
-            echo "date" >> $name
-
-            if [[ $first_submit == 1 ]]; then
-                # Submit first job w/o dependency
-                echo "Submitting $name"
-                first_submit=0
-                job=`qsub $name`
-            elif [[ $count -lt $num_running_jobs ]]; then
-                echo "Submitting $name after ${job: -6}"
-                job=`qsub --dependencies ${job: -6} $name`
-            fi
-            count=$(($count+1))
+        core=`echo $per | cut -d'_' -f 2`
+        size=`echo $per | cut -d'_' -f 3`
  
+        if [[ $count -lt $num_running_jobs ]]; then
+            read_line="${core}_${size} r"
+            write_line="${core}_${size} w"
+	    read_count=`cat complete|grep "$read_line"|wc -l`
+	    write_count=`cat complete|grep "$write_line"|wc -l`
+
+            echo "write count $write_count; read count $read_count"
+
+            if [[ $write_count -lt $repetitions || $read_count -lt $repetitions ]]; then
+                local timestamp=`date +%s`
+                local name=${per}_${timestamp}.sh
+
+                cp template.sh $name
+                cat $pdir/$per>>$name
+                # echo "rm -rf /tmp/jsm.login1.4069">>${name}
+
+                if [ "$node" -gt "16" ]
+                then 
+                    sed -i "s/ALLOCNODE/128/g" $name 
+                    sed -i "s/NNODE/$node/g" $name 
+                    sed -i "s/MYQUEUE/default/g" $name 
+                else
+                    sed -i "s/ALLOCNODE/$node/g" $name 
+                    sed -i "s/NNODE/$node/g" $name 
+                    sed -i "s/MYQUEUE/debug-cache-quad/g" $name 
+                fi
+
+
+
+                last=`sqs|grep $user|grep "IOR"|tail -1|awk '{print $1}'`
+                if [[ -z $last ]]; then
+                    echo "Submitting $per in node${node}"
+                    # bsub ${name}
+                    job=`qsub $name`
+                    count=$(($count+1))
+                else
+                    echo "Submitting $per in node${node} after $job"
+                    # bsub ${name}
+                    job=`qsub --dependencies ${job: -6} $name`
+                    count=$(($count+1))
+                fi
+            fi
+
         fi
     }   
     for per in $patterns; do
--- patterns/aggregation-experiment/template/theta/template.sh
+++ patterns/aggregation-experiment/template/theta/template.sh
@@ -7,12 +7,10 @@
 #COBALT -O o$COBALT_JOBID.ior_NNODEnode
 
 
+# LD_LIBRARY_PATH=/gpfs/alpine/csc300/world-shared/gnu_build/hdf5-1.10.6.mod/build/hdf5/lib:$LD_LIBRARY_PATH
 module load cray-hdf5-parallel
 module load darshan
 
-# DARSHAN_PRELOAD=/lus/theta-fs0/software/perftools/darshan/darshan-3.2.1/lib/libdarshan.so
-
-CDIR=/projects/CSC250STDM10/tang/ior_data
 EXEC=/gpfs/mira-home/houjun/hdf5-work/ior_mod/src/ior
 
 #enable darshan dxt trace 
@@ -25,60 +23,93 @@ export DXT_ENABLE_IO_TRACE=4
 #print romio hints
 export ROMIO_PRINT_HINTS=1
 
-#two varying parameters: 1. number of aggregators, 2. read/write buffer size
+#two varying parameters: 1. number of aggregators, 2. stripe size 
 half_aggr=$((NNODE/2))
 quat_aggr=$((NNODE/4))
 eqal_aggr=NNODE
 doul_aggr=$((NNODE*2))
 qudr_aggr=$((NNODE*4))
 
+
 naggrs="$doul_aggr $qudr_aggr $eqal_aggr $half_aggr $quat_aggr"
 stripe_sizes="1m 2m 4m 8m 16m 32m 64m 128m"
 
-date
+#target repetitions
+target_repetitions=9
+check=0
 
 ior(){
     local i=$1
     local ncore=$2
     local burst=$3
-    local stripe_size=$4
  
     #check file size to determine alignment setting
     size="${burst//k}"
     fileSize=$(($size*$ncore*NNODE/1024))
-
     if [[ $fileSize -ge 16 ]]; then
         align=16m
     else
         align=1m
     fi
- 
-    rdir=result_${ncore}_${burst}_${stripe_size}
-    mkdir -p $rdir 
-    mkdir -p $CDIR
-    
-    #set stripe size
-    lfs setstripe -c 128 -S ${stripe_size} $CDIR
- 
-    write(){
+
+    #check file size to determine default lustre striping, by following nersc recommendation 
+    DDIR=/projects/CSC250STDM10/tang/ior_data/ior_${ncore}_${burst}_default
+    if [[ ! -d $DDIR ]]; then
+        mkdir -p $DDIR
+        fGB=$(($fileSize/1024))
+        small="-c 8 -S 1m $DDIR"
+        medium="-c 24 -S 1m $DDIR"
+        large="-c 72 -S 1m $DDIR"
+        if [[ $fGB -le 10 ]]; then
+            Lustre_Default=$small 
+        elif [[ $fGB -gt 10 && $fGB -le 100 ]]; then
+            Lustre_Default=$medium
+        elif [[ $fGB -gt 10 && $fGB -le 100 ]]; then
+            Lustre_Deafult=$large 
+        fi 
+
+        lfs setstripe $Lustre_Default
+    	echo "lustre default, ${ncore}_${burst}, $fGB, $Lustre_Default"
+    fi
+
+    #check read/write repetitions, target: 9 repetitions, per write: 3
+    read_line="${ncore}_${burst} r"
+    write_line="${ncore}_${burst} w"
+    read_count=`cat complete|grep "$read_line"|wc -l`
+    write_count=`cat complete|grep "$write_line"|wc -l`
+
+    #track results
+    rdir=result_${ncore}_${burst}
+    mkdir -p $rdir
+
+    ior_write(){
         ind_write(){
             #independent write
             #flush data in data transfer, before file close
-            let NPROC=NNODE*$ncore
-            cmd="aprun -N $ncore -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -e -w -o $CDIR/ind_${i}_${ncore}_${burst}_${stripe_size}_f --hdf5.collectiveMetadata"
-            echo $cmd
-            # $cmd
-            $cmd &>>$rdir/ind_${ncore}_${burst}_${stripe_size}_f
+	    local stripe_size=$1
+
+            CDIR=/projects/CSC250STDM10/tang/ior_data/ior_${ncore}_${burst}_${stripe_size}
+	    if [[ ! -d $CDIR ]]; then
+                mkdir -p $CDIR
+                lfs setstripe -c 128 -S ${stripe_size} $CDIR
+	    fi
+
+	    let NPROC=NNODE*$ncore
+            export LD_PRELOAD=/usr/common/software/darshan/3.2.1/lib/libdarshan.so
+            srun -N NNODE -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -e -w -o $CDIR/ind_${i}_${ncore}_${burst}_${stripe_size}_f&>>$rdir/ind_${ncore}_${burst}_${stripe_size}_f 
+            export LD_PRELOAD=""
         }
 
         col_write(){
             local naggr=$1
+            local stripe_size=$2
 
+	    CDIR=/projects/CSC250STDM10/tang/ior_data/ior_${ncore}_${burst}_${stripe_size}
+	
+	    #load romio hints
+            # export ROMIO_HINTS=$hfile
             hfile=$rdir/aggr_${naggr}
             cp hints/aggr_${naggr} $hfile  
-
-            #load romio hints
-            # export ROMIO_HINTS=$hfile
             hvalue=`cat $hfile`
             echo "$hvalue"
             export MPICH_MPIIO_HINTS="*:$hvalue"
@@ -86,95 +117,124 @@ ior(){
 
             #flush data in data transfer, before file close 
             let NPROC=NNODE*$ncore
-            cmd="aprun -N $ncore -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -e -w -o $CDIR/col_${i}_${ncore}_${burst}_${stripe_size}_${naggr}_f --hdf5.collectiveMetadata"
-            echo $cmd
-            # $cmd
-            $cmd &>>$rdir/col_${ncore}_${burst}_${stripe_size}_${naggr}_f
+            export LD_PRELOAD=/usr/common/software/darshan/3.2.1/lib/libdarshan.so
+            srun -N NNODE -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -e -w -o $CDIR/col_${i}_${ncore}_${burst}_${naggr}_${stripe_size}_f&>>$rdir/col_${ncore}_${burst}_${naggr}_${stripe_size}_f
+            export LD_PRELOAD=""
         }
 
         default_write(){
             #load romio hints
-            # cp hints/default $rdir/.
-            # export ROMIO_HINTS=$rdir/default
-            # hvalue=`cat $rdir/default`
-            # echo "$hvalue"
+            # export ROMIO_HINTS=" "
             export MPICH_MPIIO_HINTS="*:"
             echo $MPICH_MPIIO_HINTS
 
             #flush data in data transfer, before file close 
             let NPROC=NNODE*$ncore
-            cmd="aprun -N $ncore -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -e -w -o $CDIR/col_${i}_${ncore}_${burst}_${stripe_size}_default_f --hdf5.collectiveMetadata"
-            echo $cmd
-            # $cmd
-            $cmd &>>$rdir/col_${ncore}_${burst}_${stripe_size}_default_f
+            export LD_PRELOAD=/usr/common/software/darshan/3.2.1/lib/libdarshan.so
+            srun -N NNODE -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -e -w -o $DDIR/col_${i}_${ncore}_${burst}_default_f&>>$rdir/col_${ncore}_${burst}_default_f
+            export LD_PRELOAD=""
         }
 
 
-        for naggr in $naggrs; do
-            col_write $naggr
+	for stripe_size in $stripe_sizes; do
+	    ind_write $stripe_size
+	done
+	  
+
+	for naggr in $naggrs; do
+            for stripe_size in $stripe_sizes; do
+                col_write $naggr $stripe_size
+	    done
         done
 
         default_write
-        ind_write
     }
     
-
-    read(){
+    ior_read(){
         ind_read(){
             #independent read
-            let NPROC=NNODE*$ncore
-            cmd="aprun -N $ncore -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -r -Z -o $CDIR/ind_${i}_${ncore}_${burst}_${stripe_size}_f --hdf5.collectiveMetadata"
-            echo $cmd
-            # $cmd
-            $cmd &>>ind_${ncore}_${burst}_${stripe_size}_r
-        }
+	    local stripe_size=$1
+            CDIR=/projects/CSC250STDM10/tang/ior_data/ior_${ncore}_${burst}_${stripe_size}
 
+	    let NPROC=NNODE*$ncore
+            export LD_PRELOAD=/usr/common/software/darshan/3.2.1/lib/libdarshan.so
+            srun -N NNODE -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -r -o $CDIR/ind_${i}_${ncore}_${burst}_${stripe_size}_f&>>$rdir/ind_${ncore}_${burst}_${stripe_size}_r   
+            export LD_PRELOAD=""
+        }
         col_read(){ 
             local naggr=$1
-            local buffer=$stripe_size 
-
-            hfile=$rdir/aggr_${naggr}
-            #load romio hints
+            local stripe_size=$2
+	    CDIR=/projects/CSC250STDM10/tang/ior_data/ior_${ncore}_${burst}_${stripe_size}
+	    #load romio hints
             # export ROMIO_HINTS=$rdir/aggr_${naggr}_${buffer}
+            hfile=$rdir/aggr_${naggr}
+            cp hints/aggr_${naggr} $hfile  
             hvalue=`cat $rdir/aggr_${naggr}`
             echo "$hvalue"
             export MPICH_MPIIO_HINTS="*:$hvalue"
             echo $MPICH_MPIIO_HINTS
 
             let NPROC=NNODE*$ncore
-            cmd="aprun -N $ncore -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -r -Z -o $CDIR/col_${i}_${ncore}_${burst}_${stripe_size}_${naggr}_f --hdf5.collectiveMetadata"
-            echo $cmd
-            # $cmd
-            $cmd &>>$rdir/col_${ncore}_${burst}_${stripe_size}_${naggr}_r
+            export LD_PRELOAD=/usr/common/software/darshan/3.2.1/lib/libdarshan.so
+            srun -N NNODE -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -r -o $CDIR/col_${i}_${ncore}_${burst}_${naggr}_${stripe_size}_f&>>$rdir/col_${ncore}_${burst}_${naggr}_${stripe_size}_r   
+            export LD_PRELOAD=""
         }
  
         default_read(){ 
             #load romio hints
-            # export ROMIO_HINTS=$rdir/default
-
+            # export ROMIO_HINTS=" "
             export MPICH_MPIIO_HINTS="*:"
             echo $MPICH_MPIIO_HINTS
 
             let NPROC=NNODE*$ncore
-            cmd="aprun -N $ncore -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -r -Z -o $CDIR/col_${i}_${ncore}_${burst}_${stripe_size}_default_f --hdf5.collectiveMetadata"
-            echo $cmd
-            # $cmd
-            $cmd &>>$rdir/col_${ncore}_${burst}_${stripe_size}_default_r
+            export LD_PRELOAD=/usr/common/software/darshan/3.2.1/lib/libdarshan.so
+            srun -N NNODE -n $NPROC $EXEC -b $burst -t $burst -i 1 -v -v -v -k -a HDF5 -J $align -c -r -o $DDIR/col_${i}_${ncore}_${burst}_default_f&>>$rdir/col_${ncore}_${burst}_default_r     
+            export LD_PRELOAD=""
         }
- 
-        for naggr in $naggrs; do
-            col_read $naggr
+
+
+        for stripe_size in $stripe_sizes; do
+	    ind_read $stripe_size
+	done
+
+	for naggr in $naggrs; do
+	    for stripe_size in $stripe_sizes; do 
+                col_read $naggr $stripe_size
+	    done
         done
-    
+
         default_read
-        ind_read
-  
     }
+    
+    if [[ $read_count -lt $target_repetitions ]]; then
+        if [[ -f $DDIR/col_${i}_${ncore}_${burst}_default_f && $check -ne 2 ]]; then
+            ior_read 
+            echo $read_line>>complete
+            read_count=$(($read_count+1))
+            check=1
+        fi 
+    fi
 
-    write
-    read
+ 
+    if [[ $check -eq 0 || $check -eq 2 ]]; then
+        if [[ $write_count -lt $target_repetitions ]]; then
+           ior_write
+           write_count=$(($write_count+1))
+           echo $write_line>>complete
+           check=2
+        elif [[ $read_count -lt $target_repetitions && $i -eq 1 ]]; then
+            ior_write
+            write_count=$(($write_count+1))
+            echo $write_line>>complete
+            check=2 
+        fi
+    fi
 
-    #clean the run
-    rm -rf $CDIR
+    if [[ $read_count -ge $target_repetitions ]]; then
+        rm -rf /projects/CSC250STDM10/tang/ior_data/ior_${ncore}_${burst}_* 
+    fi
+   
+    echo "bing, read count, $read_count, write count, $write_count" 
+
 }
 
